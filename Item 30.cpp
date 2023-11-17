d2391d917941e8aa8d0484a3dd16e0cdbc9ba52e
The-Lonesome-Coder <the.lonesome.coder@gmail.com>
2023-11-18 06:15:25 +0800

    Understand the ins and outs of inlining

---
+++ Item 30.cpp
@@ -0,0 +1,10 @@
+/**
+ * Understand the ins and outs of inlining.
+*/
+
+/**
+ * With compiler getting stronger and stronger in optimization, the usage of inline has changed.
+ * See: Is there still a use for inline? (https://stackoverflow.com/questions/29796264/is-there-still-a-use-for-inline)
+ *
+ * Therefore, this item is deprecated.
+*/
