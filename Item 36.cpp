5620569deac73e442c21d4ebd150ca9b93be5776
The-Lonesome-Coder <the.lonesome.coder@gmail.com>
2023-11-20 12:32:07 +0800

    Never redefine an inherited non-virtual function

---
+++ Item 36.cpp
@@ -0,0 +1,8 @@
+/**
+ * Never redefine an inherited non-virtual function.
+*/
+
+/**
+ * If you redefine a non-virtual function, there is a contradiction in your design.
+ * If the same functions in different class needs to behave differently, it should be virtual.
+*/
