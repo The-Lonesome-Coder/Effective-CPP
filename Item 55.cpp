a8be6a0daa4fb9546d2e3e59d2248777f7d56e25
The-Lonesome-Coder <the.lonesome.coder@gmail.com>
2023-11-28 02:00:41 +0800

    Familiarize yourself with boost

---
+++ Item 55.cpp
@@ -0,0 +1,15 @@
+/**
+ * Familiarize yourself with boost.
+*/
+
+/**
+ * Boost is a collection of high-quality, open source, platform- and compiler-independent libraries.
+ *
+ * Boost is both a community of C++ developers and a collection of freely downloadable C++ libraries.
+*/
+
+/**
+ * The description in this Item is outdated. Most of the categories are integrated into C++ standard library.
+ *
+ * The principle is still valid, familiarize yourself with Boost!
+*/
