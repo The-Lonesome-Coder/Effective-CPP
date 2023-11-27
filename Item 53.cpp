4daad4538d2b2e5a431a82b9b45d41930603a194
The-Lonesome-Coder <the.lonesome.coder@gmail.com>
2023-11-28 01:56:26 +0800

    Pay attention to compiler warnings

---
+++ Item 53.cpp
@@ -0,0 +1,9 @@
+/**
+ * Pay attention to compiler warnings.
+ *
+ * Take compiler warnings seriously, and strive to compile warning free at the maximum warning level
+ * supported by your compilers.
+ *
+ * Don’t become dependent on compiler warnings, because different compilers warn about different things.
+ * Porting to a new compiler may eliminate warning messages you’ve come to rely on.
+*/
