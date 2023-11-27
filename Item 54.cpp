a6f4ec75c15a452f41f54c7a8cf72e7752f97292
The-Lonesome-Coder <the.lonesome.coder@gmail.com>
2023-11-28 01:58:34 +0800

    Familiarize yourself with the standard library

---
+++ Item 54.cpp
@@ -0,0 +1,9 @@
+/**
+ * Familiarize yourself with the standard library.
+*/
+
+/**
+ * The description in this Item is outdated. Most of TR1 is integrated into standard library.
+ *
+ * The principle is still valid, familiarize yourself with the standard library!
+*/
